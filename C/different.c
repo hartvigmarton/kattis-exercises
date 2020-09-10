3d287da468ac56806a9a9b1affe60cf42b880d8f
Márton Hartvig <34651552+hartvigmarton@users.noreply.github.com>
2020-09-10 10:09:09 +0200

    problem description added

--- C/different.c
+++ C/different.c
@@ -1,3 +1,12 @@
+/*
+Write a program that computes the difference between non-negative integers.
+
+Input
+Each line of the input consists of a pair of integers. Each integer is between 0 and 1015 (inclusive). The input is terminated by end of file.
+
+Output
+For each pair of integers in the input, output one line, containing the absolute value of their difference.
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
